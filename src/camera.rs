961d2ee328ed31ac6ae06a4b75bfe9d23a4ca229
wesfly <175718527+wesfly@users.noreply.github.com>
2025-05-10 12:43:56 +0200

    fix scroll sensitivity

--- src/camera.rs
+++ src/camera.rs
@@ -139,8 +139,7 @@ impl CameraController {
 
     pub fn process_scroll(&mut self, delta: &MouseScrollDelta) {
         self.scroll = match delta {
-            // I'm assuming a line is about 100 pixels
-            MouseScrollDelta::LineDelta(_, scroll) => -scroll * 0.5,
+            MouseScrollDelta::LineDelta(_, scroll) => -scroll * 20.0,
             MouseScrollDelta::PixelDelta(PhysicalPosition { y: scroll, .. }) => -*scroll as f32,
         };
     }
